868437e135d12e5673c0720a5973e363cd5d7066
Lalit kumar Prajapati <146929824+bylalit@users.noreply.github.com>
2025-11-10 21:24:02 +0530

    Enhance isLoggedin middleware with token verification
    Added error handling and token data extraction in isLoggedin middleware.

--- middleware/isLoggedin.js
+++ middleware/isLoggedin.js
@@ -1,12 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 const isLoggedin = async (req, res, next) => {
+    
     try {
         const token = req.cookies.token;
         if (!token) return res.redirect('/admin/');
 
         const tokenData = jwt.verify(token, process.env.JWT_SECRET);
         // console.log(tokenData);
+        
         req.id = tokenData.id;
         req.role = tokenData.role;
         req.fullname = tokenData.fullname;
@@ -15,6 +17,8 @@ const isLoggedin = async (req, res, next) => {
         console.error(error);
         res.status(500).send('Something went wrong');
     }
+    
 };
 
 module.exports = isLoggedin;
+
